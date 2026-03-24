767ca040693370666a943d6f7de435e566c13446
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2026-03-24 12:14:13 +0700

    Tạo entity cho bảng HoaDon

--- app/src/main/java/com/example/quanlycaphe/entity/HoaDon.java
+++ app/src/main/java/com/example/quanlycaphe/entity/HoaDon.java
@@ -1,4 +1,52 @@
 package com.example.quanlycaphe.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "HoaDon",
+        foreignKeys = {
+
+                @ForeignKey(entity = NhanVien.class,
+                        parentColumns = "MaNV",
+                        childColumns = "MaNV",
+                        onDelete = ForeignKey.SET_NULL),
+
+                @ForeignKey(entity = KhachHang.class,
+                        parentColumns = "MaKH",
+                        childColumns = "MaKH",
+                        onDelete = ForeignKey.SET_NULL),
+
+                @ForeignKey(entity = BanAn.class,
+                        parentColumns = "id",
+                        childColumns = "idBan")
+        }
+)
 public class HoaDon {
+
+    @PrimaryKey
+    @NonNull
+    public String MaHD;
+
+    public String NgayLap;
+
+    public String MaNV;
+
+    public String MaKH;
+
+    public int idBan;
+
+    public Double TongTien;
+
+    public Double GiamGia;
+
+    public Double ThanhTien;
+
+    public String TrangThai;
+
+    public Integer DiemSuDung;
+
+    public String GhiChu;
 }
