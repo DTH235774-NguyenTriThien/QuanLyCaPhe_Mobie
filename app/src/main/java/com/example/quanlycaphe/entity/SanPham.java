d744f0ba75f8f1bd00f0a9dd9ccfce2db0ef2363
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2026-03-24 12:13:24 +0700

    Tạo entity cho bảng SanPham

--- app/src/main/java/com/example/quanlycaphe/entity/SanPham.java
+++ app/src/main/java/com/example/quanlycaphe/entity/SanPham.java
@@ -1,4 +1,23 @@
 package com.example.quanlycaphe.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "SanPham")
 public class SanPham {
+
+    @PrimaryKey
+    @NonNull
+    public String MaSP;
+
+    public String TenSP;
+
+    public String LoaiSP;
+
+    public Double DonGia;
+
+    public String TrangThai;
+
+    public String ImagePath;
 }
