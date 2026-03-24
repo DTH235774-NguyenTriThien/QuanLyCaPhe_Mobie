732b210a645e2205e6d3e7be39c27ac9513cb21a
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2026-03-24 12:14:28 +0700

    Tạo entity cho bảng ChiTietHoaDon

--- app/src/main/java/com/example/quanlycaphe/entity/ChiTietHoaDon.java
+++ app/src/main/java/com/example/quanlycaphe/entity/ChiTietHoaDon.java
@@ -1,4 +1,37 @@
 package com.example.quanlycaphe.entity;
 
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "ChiTietHoaDon",
+        foreignKeys = {
+
+                @ForeignKey(entity = HoaDon.class,
+                        parentColumns = "MaHD",
+                        childColumns = "MaHD",
+                        onDelete = ForeignKey.CASCADE),
+
+                @ForeignKey(entity = SanPham.class,
+                        parentColumns = "MaSP",
+                        childColumns = "MaSP")
+        }
+)
 public class ChiTietHoaDon {
+
+    @PrimaryKey(autoGenerate = true)
+    public int ChiTietID;
+
+    public String MaHD;
+
+    public String MaSP;
+
+    public int SoLuong;
+
+    public Double DonGia;
+
+    public Double ThanhTien;
+
+    public String GhiChu;
 }
