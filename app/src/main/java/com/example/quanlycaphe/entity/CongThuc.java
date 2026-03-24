bf22d613e1e2886a37f2012ab5ba382784ce83ca
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2026-03-24 12:14:04 +0700

    Tạo entity cho bảng CongThuc

--- app/src/main/java/com/example/quanlycaphe/entity/CongThuc.java
+++ app/src/main/java/com/example/quanlycaphe/entity/CongThuc.java
@@ -1,4 +1,33 @@
 package com.example.quanlycaphe.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+
+@Entity(
+
+        tableName = "CongThuc",
+        primaryKeys = {"MaSP","MaNL"},
+        foreignKeys = {
+
+                @ForeignKey(entity = SanPham.class,
+                        parentColumns = "MaSP",
+                        childColumns = "MaSP",
+                        onDelete = ForeignKey.CASCADE),
+
+                @ForeignKey(entity = NguyenLieu.class,
+                        parentColumns = "MaNL",
+                        childColumns = "MaNL",
+                        onDelete = ForeignKey.CASCADE)
+        }
+)
 public class CongThuc {
+
+    @NonNull
+    public String MaSP;
+
+    @NonNull
+    public String MaNL;
+
+    public Double SoLuong;
 }
