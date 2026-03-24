d939954adefae6ab827154a33b9eec7107764dcf
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2026-03-24 12:14:49 +0700

    Tạo entity cho bảng InventoryMovements

--- app/src/main/java/com/example/quanlycaphe/entity/InventoryMovements.java
+++ app/src/main/java/com/example/quanlycaphe/entity/InventoryMovements.java
@@ -1,4 +1,28 @@
 package com.example.quanlycaphe.entity;
 
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "InventoryMovements",
+        foreignKeys = @ForeignKey(
+                entity = NguyenLieu.class,
+                parentColumns = "MaNL",
+                childColumns = "MaNL",
+                onDelete = ForeignKey.SET_NULL
+        )
+)
 public class InventoryMovements {
+
+    @PrimaryKey(autoGenerate = true)
+    public int MovementID;
+
+    public String MaNL;
+
+    public Double ChangeQty;
+
+    public String MovementType;
+
+    public String Timestamp;
 }
