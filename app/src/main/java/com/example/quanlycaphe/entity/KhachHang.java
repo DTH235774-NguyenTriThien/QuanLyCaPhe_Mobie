de76332016a2f230b7c378f0cfef2cd80754ede8
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2026-03-24 12:06:04 +0700

    Tạo entity cho bảng KhachHang

--- app/src/main/java/com/example/quanlycaphe/entity/KhachHang.java
+++ app/src/main/java/com/example/quanlycaphe/entity/KhachHang.java
@@ -1,4 +1,18 @@
 package com.example.quanlycaphe.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "KhachHang")
 public class KhachHang {
+
+    @PrimaryKey
+    @NonNull
+    public String MaKH;
+
+    public String TenKH;
+    public String SDT;
+
+    public int DiemTichLuy;
 }
