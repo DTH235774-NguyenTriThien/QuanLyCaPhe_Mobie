6454f88ee619b86346b910c08960e66530bffe22
DTH235774-NguyenTriThien <jackmian2008@gmail.com>
2026-03-24 12:13:42 +0700

    Tạo entity cho bảng NguyenLieu

--- app/src/main/java/com/example/quanlycaphe/entity/NguyenLieu.java
+++ app/src/main/java/com/example/quanlycaphe/entity/NguyenLieu.java
@@ -1,4 +1,18 @@
 package com.example.quanlycaphe.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "NguyenLieu")
 public class NguyenLieu {
+
+    @PrimaryKey
+    @NonNull
+    public String MaNL;
+
+    public String TenNL;
+    public String DonVi;
+
+    public Double SoLuongTon;
 }
